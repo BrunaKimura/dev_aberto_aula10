bea78f5d3c401735e066e5b660e6c44ebebd6822
brunakimura <brunamk@al.insper.edu.br>
2020-04-06 19:56:18 -0300

    adicionando as dependências

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@ from setuptools import setup
 
 setup(name='dev_aberto_brunamk',
 version='0.1',
-packages=['dev_aberto']
+packages=['dev_aberto'],
+author='Bruna',
+python_requires='>=3',
+install_requires=[
+'requests>=2.21.0'
+]
 )
